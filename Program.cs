2665251fe66b9e0f4e7a16d842bb12016d1befb1
Jean Martin <jeanmartinrafael@gmail.com>
2026-05-21 22:17:02 -0400

    Primera practica Web API

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Endpoint  principal
+app.MapGet("/", () => "¡Bienvenido a Jean web api!");
+
+// Endpoint con mi nombre y apellido
+app.MapGet("/jean-pangol", () =>
+{
+    var info = new Dictionary<string, string>
+    {
+        { "Nombre",   "Jean" },
+        { "Apellido", "Pangol" }
+    };
+    return string.Join("  ", info.Select(x => $"{x.Key}: {x.Value}"));
+});
+
+app.Run();
